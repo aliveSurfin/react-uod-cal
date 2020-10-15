91a504691fe395a300570e1995c291df22c4eaab
alivesurfin <alivesurfin@gmail.com>
2020-10-15 04:48:14 +0100

    Updates

---
+++ precache-manifest.bac3a562844ca7e41290bb861fd74e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1be84c37e4d3e203732b4c4f8ebf79",
+    "url": "/react-uod-cal/index.html"
+  },
+  {
+    "revision": "65f8d76a3bcefb302cb4",
+    "url": "/react-uod-cal/static/css/main.1ceff1e6.chunk.css"
+  },
+  {
+    "revision": "2be1d1a05a6039688d25",
+    "url": "/react-uod-cal/static/js/2.b43edb0c.chunk.js"
+  },
+  {
+    "revision": "cf34a5ed6b954952b3e9a6b28469a0c5",
+    "url": "/react-uod-cal/static/js/2.b43edb0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f8d76a3bcefb302cb4",
+    "url": "/react-uod-cal/static/js/main.1330bbc6.chunk.js"
+  },
+  {
+    "revision": "d4e5c454dff065e032bc",
+    "url": "/react-uod-cal/static/js/runtime-main.63660683.js"
+  }
+]);
